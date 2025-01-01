9c63eba61cf6d78cdcbd6abad8f7afd129bd6ff9
Egor <egorka_lolka23@mail.ru>
2025-01-01 19:40:01 +0400

    first commit

---
+++ src/main/java/ru/korovin/TestPlatform/TestPlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.korovin.TestPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPlatformApplication.class, args);
+	}
+
+}
